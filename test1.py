582ac8b3fd0eaa00015aa36f4e1809058ef5365c
Simercher <62394051+Simercher@users.noreply.github.com>
2023-02-12 20:37:52 +0800

    4

--- test1.py
+++ test1.py
@@ -1,8 +1,8 @@
 import numpy as np
-# import sys
-# sys.path.append("/usr/local/lib/python3.8/site-packages")
+import sys
+sys.path.append("/usr/local/lib/python3.8/site-packages")
 import cv2
-# import serial
+import serial
 
 def map(turn):
     return (turn - 32) * (120 - 0) / (40 - 32)
@@ -60,6 +60,51 @@ def calTurn(img):
     print(turn)
     return img, turn
 
+def canny(img):
+    # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # kernel_size = 3
+    # img = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+    low_threshold = 75
+    high_threshold = 150
+    img = cv2.Canny(img, low_threshold, high_threshold)
+    return img
+
+def getLane(frame):
+    turn_list = []
+    img_HSV = toHSV(frame.copy())
+    img = getHsvMask(img_HSV)
+    edges = canny(img.copy())
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30,minLineLength=20,maxLineGap=10)
+    try:
+        for line in lines:
+            # newlines1 = lines[:, 0, :]
+            # print ("line["+str(i-1)+"]=",line)
+            if line is None:
+                continue
+            x1,y1,x2,y2 = line[0]  
+            cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2) 
+
+            x1 = float(x1)
+            x2 = float(x2)
+            y1 = float(y1)
+            y2 = float(y2)
+            # print ("x1=%s,x2=%s,y1=%s,y2=%s" % (x1, x2, y1, y2))
+            if x2 - x1 == 0:
+                result=90
+            elif y2 - y1 == 0 :
+                result=0
+            else:
+                k = -(y2 - y1) / (x2 - x1)
+                result = np.arctan(k) * 57.29577
+            turn_list.append(result)
+    except Exception:
+        pass
+    cv2.imshow("line_detect",frame)
+    try:
+        return sum(turn_list) / len(turn_list)
+    except Exception:
+        return sum(turn_list) / (len(turn_list)+1)
+
 def main():
     serial_port = serial.Serial(
     port="/dev/ttyUSB0",
@@ -75,16 +120,15 @@ def main():
     turn_list = []
     while True:
         ret, frame = cap.read()
-        frame = cv2.resize(frame, (64, 48))
+        # frame = cv2.resize(frame, (64, 48))
         # print(frame.shape[0], frame.shape[1])
         if ret:
-            cv2.imshow('frame',frame)
-            frame, turn = calTurn(frame)
-            turn_list.append(turn)
-            cv2.imshow('video',frame)
+            turn_list.append(getLane(frame))
+            # cv2.imshow('video',frame)
             if len(turn_list) >= 6 :
                 turn = sum(turn_list) / len(turn_list)
                 turn_list.clear()
+                print(turn)
                 try:
                     # Send a message to the Arduino
                     serial_port.open()
