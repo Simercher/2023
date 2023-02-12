5d2179135955de79f2a0735c20f54664e35d58ad
Simercher <62394051+Simercher@users.noreply.github.com>
2023-02-12 18:23:15 +0800

    3

--- test1.py
+++ test1.py
@@ -1,24 +1,24 @@
 import numpy as np
-import sys
-sys.path.append("/usr/local/lib/python3.8/site-packages")
+# import sys
+# sys.path.append("/usr/local/lib/python3.8/site-packages")
 import cv2
-import serial
+# import serial
 
 def map(turn):
-    return (turn - 320) * (120 - 0) / (470 - 320)
+    return (turn - 32) * (120 - 0) / (40 - 32)
 
 def drawLine(img, left_deviation, right_deviation):
-    cv2.line(img, (0, 100), (300, 100), (0,255,0),3)
-    cv2.line(img, (340, 100), (640, 100), (0,255,0),3)
-    cv2.line(img, (320, 370), (320, 430), (255,255,255),3)
-    cv2.line(img, (0, 70), (0, 130), (0,255,0),3)
-    cv2.line(img, (300, 70), (300, 130), (0,255,0),3)
-    cv2.line(img, (340, 70), (340, 130), (0,255,0),3)
-    cv2.line(img, (640, 70), (640, 130), (0,255,0),3)
-    cv2.line(img, (left_deviation, 80), (left_deviation, 120), (0,0,255),3)
-    cv2.line(img, (right_deviation, 80), (right_deviation, 120), (0,0,255),3)
+    cv2.line(img, (0, 10), (30, 10), (0,255,0),3)
+    cv2.line(img, (34, 10), (64, 10), (0,255,0),3)
+    cv2.line(img, (32, 37), (32, 43), (255,255,255),3)
+    cv2.line(img, (0, 7), (0, 13), (0,255,0),3)
+    cv2.line(img, (30, 7), (30, 13), (0,255,0),3)
+    cv2.line(img, (34, 7), (34, 13), (0,255,0),3)
+    cv2.line(img, (64, 7), (64, 13), (0,255,0),3)
+    cv2.line(img, (left_deviation, 8), (left_deviation, 12), (0,0,255),3)
+    cv2.line(img, (right_deviation, 8), (right_deviation, 12), (0,0,255),3)
     turn = int((right_deviation + left_deviation)/2)
-    cv2.line(img, (320, 400), (turn, 400), (255,0,0),3)
+    cv2.line(img, (32, 40), (turn, 40), (255,0,0),1)
     return turn
 
 def toHSV(img):
@@ -27,14 +27,8 @@ def toHSV(img):
      return img_HSV
 
 def getHsvMask(img):
-     h_min = cv2.getTrackbarPos('HUE Min', 'TrackerBar')
-     h_max = cv2.getTrackbarPos('HUE Max', 'TrackerBar')
-     s_min = cv2.getTrackbarPos('SAT Min', 'TrackerBar')
-     s_max = cv2.getTrackbarPos('SAT Max', 'TrackerBar')
-     v_min = cv2.getTrackbarPos('VALUE Min', 'TrackerBar')
-     v_max = cv2.getTrackbarPos('VALUE Max', 'TrackerBar')
-     lowerWhite = np.array([h_min, s_min, v_min])
-     upperWhite = np.array([h_max, s_max, v_max])
+     lowerWhite = np.array([0, 0, 221])
+     upperWhite = np.array([180, 30, 255])
      maskWhite = cv2.inRange(img, lowerWhite, upperWhite)
 
      return maskWhite
@@ -47,17 +41,17 @@ def calTurn(img):
 #     high_threshold = 150
 #     img = cv2.Canny(img, low_threshold, high_threshold)
     img_HSV = toHSV(img)
-    HSV_mask = getHsvMask(img_HSV)
+    img = getHsvMask(img_HSV)
     
     left_deviation = 0
-    for i in range(300,0,-1):
-        if (img[100][i] == 255):
+    for i in range(30,0,-1):
+        if (img[10][i] == 255):
             left_deviation = i
             break
     
-    right_deviation = 640
-    for i in range(340,640):
-        if (img[100][i] == 255):
+    right_deviation = 64
+    for i in range(34,64):
+        if (img[10][i] == 255):
             right_deviation = i
             break
     turn = drawLine(img, left_deviation, right_deviation)
@@ -75,13 +69,16 @@ def main():
     stopbits=serial.STOPBITS_ONE,
     )
     
-    cap = cv2.VideoCapture(-1)
-    width = 480
-    height = 640
+    cap = cv2.VideoCapture(1)
+    width = 48
+    height = 64
     turn_list = []
     while True:
         ret, frame = cap.read()
+        frame = cv2.resize(frame, (64, 48))
+        # print(frame.shape[0], frame.shape[1])
         if ret:
+            cv2.imshow('frame',frame)
             frame, turn = calTurn(frame)
             turn_list.append(turn)
             cv2.imshow('video',frame)
@@ -92,9 +89,9 @@ def main():
                     # Send a message to the Arduino
                     serial_port.open()
                     msg = '0 0'
-                    if turn > 320:
+                    if turn > 32:
                         msg = str(int(120 - map(turn))) + '&120\n'
-                    elif turn < 320:
+                    elif turn < 32:
                         msg = '120&' + str(int(120 + map(turn))) + '\n'
                     else:
                         msg = '120&120\n'
@@ -107,6 +104,7 @@ def main():
                     print("Error: " + str(exception_error))
                 finally:
                     serial_port.close()
+                    pass
         if cv2.waitKey(10) == ord('q'):
              break
     cap.release()
