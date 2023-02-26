c8cd6fc376075be2278075b0ad0d0b912564a00c
Simercher <62394051+Simercher@users.noreply.github.com>
2023-02-26 12:39:18 +0800

    Add files via upload

---
+++ PID_VelocityControl.py
@@ -0,0 +1,36 @@
+class PID_VelocityControl():
+    def __init__(self, kp = 0, ki = 0, kd = 0, d_time = 100, max_velocity = 100, min_velocity = 0, max_PID = 170, min_PID = 0):
+        # self.encoder = 0
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.error = 0
+        self.pre_error = 0
+        self.P = 0
+        self.I = 0
+        self.D = 0
+        self.PID = 0
+        self.d_time = d_time
+        self.velocity = 0
+        self.max_Velocity = max_velocity
+        self.min_Velocity = min_velocity
+        self.max_PID = max_PID
+        self.min_PID = min_PID
+    def calPID(self, target, encoder):
+        self.error = target - encoder
+        self.P = self.kp * self.error
+        self.I += self.ki * (self.error * self.d_time)
+        self.D = self.kd * ((self.error - self.pre_error) / self.d_time)
+        self.pre_error = self.error
+
+        self.PID = self.P + self.I + self.D
+    def setVelocity(self):
+        self.convertPID()
+        self.velocity += self.PID
+        if self.velocity >= self.max_Velocity:
+            self.velocity = self.max_Velocity
+        elif self.velocity < self.min_Velocity:
+            self.velocity = self.min_Velocity
+        return self.velocity
+    def convertPID(self):
+        self.PID = (self.PID - self.min_PID) / (self.max_PID - self.min_PID) * (self.max_Velocity - self.min_Velocity)
